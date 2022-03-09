f61d619b2605a3cc3ef8ccc9086049a7ca403776
Parkavan Illavalagan <parkavai@ulrik.uio.no>
2022-03-09 19:19:00 +0100

    Gjort det mulig for testing

--- app/src/main/java/com/example/himmeltitting/MainActivity.kt
+++ app/src/main/java/com/example/himmeltitting/MainActivity.kt
@@ -2,15 +2,47 @@ package com.example.himmeltitting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.example.himmeltitting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.fetchLocation(40.7127,74.0059,"2022-03-08", 2, 25.00,  "05:00")
+        viewModel.getLocation().observe(this){
+            Log.d("Testing", "Day-1")
+            Log.d("High-Moon ", it.time!![0].high_moon.toString())
+            Log.d("Low-Moon ", it.time[0].low_moon.toString())
+            Log.d("Moon-phase ", it.time[0].moonphase.toString())
+            Log.d("Moon-position", it.time[0].moonposition.toString())
+            Log.d("Moon-Rise", it.time[0].moonrise.toString())
+            Log.d("Moon-shadow", it.time[0].moonshadow.toString())
+            Log.d("Solar-midnight", it.time[0].solarmidnight.toString())
+            Log.d("Solar-noon", it.time[0].solarnoon.toString())
+            Log.d("Sun-Rise", it.time[0].sunrise.toString())
+            Log.d("Sun-set", it.time[0].sunset.toString())
+
+            // Hente data med noen dager fremover
+            Log.d("Testing", "Day-2")
+            Log.d("High-Moon2 ", it.time[1].high_moon.toString())
+            Log.d("Low-Moon2 ", it.time[1].low_moon.toString())
+            Log.d("Moon-phase2 ", it.time[1].moonphase.toString())
+            Log.d("Moon-position2", it.time[1].moonposition.toString())
+            Log.d("Moon-Rise2", it.time[1].moonrise.toString())
+            Log.d("Moon-shadow2", it.time[1].moonshadow.toString())
+            Log.d("Solar-midnight2", it.time[1].solarmidnight.toString())
+            Log.d("Solar-noon2", it.time[1].solarnoon.toString())
+            Log.d("Sun-Rise2", it.time[1].sunrise.toString())
+            Log.d("Sun-set2", it.time[1].sunset.toString())
+        }
 
     }
 }
--- app/src/main/java/com/example/himmeltitting/SunRiseDataSource.kt
+++ app/src/main/java/com/example/himmeltitting/SunRiseDataSource.kt
@@ -4,19 +4,27 @@ import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitString
 import com.google.gson.Gson
 
-class SunRise {
+class SunRiseDataSource {
 
     // Et gson-objekt som hjelper med aa parse data fra JSON til Kotlin
     private val gson = Gson()
-    dsadasdad
+
     // Ett eksempel paa API-kall
-    private val path = "https://in2000-apiproxy.ifi.uio.no/weatherapi/sunrise/2.0/.json?lat=40.7127&lon=-74.0059&date=2022-03-08&offset=-05:00"
+    private val path = "https://in2000-apiproxy.ifi.uio.no/weatherapi/sunrise/2.0/.json?"
 
     // Returnerer "Location" siden den er viktigst aa plukke ut
-    suspend fun fetchLocation(lat: String?, long: String?, dat: String?, offset: String?): Location? {
+    suspend fun fetchLocation(lat: Double?, long: Double?, date: String?, days: Int?, height: Double?, offset: String?): Location? {
         try {
-            val fetchObjekt = gson.fromJson(Fuel.get(path+"lat=${lat}&lon=${long}&date=${dat}&offset=${offset}").awaitString(), Base::class.java)
-            return fetchObjekt.location
+            // Forsikre at man ikke skriver over å se 15 dager fremover
+            if(days!! <= 15){
+                val parametere = "lat=${lat.toString()}&lon=${long.toString()}&date=${date}&days=${days}&height=${height}&offset=${offset}"
+                val fetchObjekt = gson.fromJson(Fuel.get(path+parametere).awaitString(), Base::class.java)
+                return fetchObjekt.location
+            }
+            else{
+                println("Value of 'days' were over 15, must be 15 or under")
+                return null
+            }
         } catch(exception: Exception) {
             println("A network request exception was thrown: ${exception.message}")
             return null
@@ -26,29 +34,34 @@ class SunRise {
 
 data class Base(val location: Location?, val meta: Meta?)
 
-data class Location(val height: String?, val latitude: String?, val longitude: String?, val time: MutableList<Time>?)
+data class Location(val height: Double?, val latitude: Double?, val longitude: Double?, val time: MutableList<Time>?)
 
 data class Meta(val licenseurl: String?)
 
-data class Time(val date: String?, val high_moon: High_moon?, val low_moon: Low_moon?, val moonphase: Moonphase?, val moonposition: Moonposition?, val moonrise: Moonrise?, val moonset: Moonset?, val moonshadow: Moonshadow?, val solarmidnight: Solarmidnight?, val solarnoon: Solarnoon?, val sunrise: Sunrise?, val sunset: Sunset?)
+/*
+* Merk at "Time"-klassen er en liste som består av alle de ulike objektene(Highmoon,Lowmoon osv)
+* Denne vil bli mest brukt for å hente de dataene vi ønsker å bruke!!!
+ */
 
-data class High_moon(val desc: String?, val elevation: String?, val time: String?)
+data class Time(val date: String?, val high_moon: Highmoon?, val low_moon: Lowmoon?, val moonphase: Moonphase?, val moonposition: Moonposition?, val moonrise: Moonrise?, val moonset: Moonset?, val moonshadow: Moonshadow?, val solarmidnight: Solarmidnight?, val solarnoon: Solarnoon?, val sunrise: Sunrise?, val sunset: Sunset?)
 
-data class Low_moon(val desc: String?, val elevation: String?, val time: String?)
+data class Highmoon(val desc: String?, val elevation: Double?, val time: String?)
 
-data class Moonphase(val desc: String?, val time: String?, val value: String?)
+data class Lowmoon(val desc: String?, val elevation: Double?, val time: String?)
 
-data class Moonposition(val azimuth: String?, val desc: String?, val elevation: String?, val phase: String?, val range: String?, val time: String?)
+data class Moonphase(val desc: String?, val time: String?, val value: Double?)
+
+data class Moonposition(val azimuth: Double?, val desc: String?, val elevation: Double?, val phase: Double?, val range: Double?, val time: String?)
 
 data class Moonrise(val desc: String?, val time: String?)
 
 data class Moonset(val desc: String?, val time: String?)
 
-data class Moonshadow(val azimuth: String?, val desc: String?, val elevation: String?, val time: String?)
+data class Moonshadow(val azimuth: Double?, val desc: String?, val elevation: Double?, val time: String?)
 
 data class Solarmidnight(val desc: String?, val elevation: String?, val time: String?)
 
-data class Solarnoon(val desc: String?, val elevation: String?, val time: String?)
+data class Solarnoon(val desc: String?, val elevation: Double?, val time: String?)
 
 data class Sunrise(val desc: String?, val time: String?)
 
