80cad2235d14623a5b8ed2cb0e330c1d84d5c9eb
Parkavan Illavalagan <parkavai@ulrik.uio.no>
2022-03-10 20:05:15 +0100

    La til flere beskrivelser

--- app/src/main/java/com/example/himmeltitting/SunRiseDataSource.kt
+++ app/src/main/java/com/example/himmeltitting/SunRiseDataSource.kt
@@ -18,7 +18,7 @@ class SunRiseDataSource {
             // Forsikre at man ikke skriver over å se 15 dager fremover
             if(days!! <= 15){
                 val parametere = "lat=${lat}&lon=${long}&date=${date}&days=${days}&height=${height}&offset=${offset}"
-                val fetchObjekt = gson.fromJson(Fuel.get(path+parametere).awaitString(), Base::class.java)
+                val fetchObjekt = gson.fromJson(Fuel.get(path+parametere).awaitString(), SunriseBase::class.java)
                 return fetchObjekt.location
             }
             else{
@@ -31,38 +31,3 @@ class SunRiseDataSource {
         }
     }
 }
-
-data class Base(val location: Location?, val meta: Meta?)
-
-data class Location(val height: Double?, val latitude: Double?, val longitude: Double?, val time: MutableList<Time>?)
-
-data class Meta(val licenseurl: String?)
-
-/*
-* Merk at "Time"-klassen er en liste som består av alle de ulike objektene(Highmoon,Lowmoon osv)
-* Denne vil bli mest brukt for å hente de dataene vi ønsker å bruke!!!
- */
-
-data class Time(val date: String?, val high_moon: Highmoon?, val low_moon: Lowmoon?, val moonphase: Moonphase?, val moonposition: Moonposition?, val moonrise: Moonrise?, val moonset: Moonset?, val moonshadow: Moonshadow?, val solarmidnight: Solarmidnight?, val solarnoon: Solarnoon?, val sunrise: Sunrise?, val sunset: Sunset?)
-
-data class Highmoon(val desc: String?, val elevation: Double?, val time: String?)
-
-data class Lowmoon(val desc: String?, val elevation: Double?, val time: String?)
-
-data class Moonphase(val desc: String?, val time: String?, val value: Double?)
-
-data class Moonposition(val azimuth: Double?, val desc: String?, val elevation: Double?, val phase: Double?, val range: Double?, val time: String?)
-
-data class Moonrise(val desc: String?, val time: String?)
-
-data class Moonset(val desc: String?, val time: String?)
-
-data class Moonshadow(val azimuth: Double?, val desc: String?, val elevation: Double?, val time: String?)
-
-data class Solarmidnight(val desc: String?, val elevation: String?, val time: String?)
-
-data class Solarnoon(val desc: String?, val elevation: Double?, val time: String?)
-
-data class Sunrise(val desc: String?, val time: String?)
-
-data class Sunset(val desc: String?, val time: String?)
