88390f55ede9a03ddcc1d66bd2b2e77d45ecba70
MathiasGretland <mathiasgretland@gmail.com>
2022-03-24 12:27:43 +0100

    NavigationMerge ferdig og klar for main

--- app/src/main/java/com/example/himmeltitting/MainActivity.kt
+++ app/src/main/java/com/example/himmeltitting/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.himmeltitting
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.himmeltitting.databinding.ActivityMainBinding
+import com.example.himmeltitting.fragments.FavoritesFragment
+import com.example.himmeltitting.fragments.InfoFragment
+import com.example.himmeltitting.fragments.Maps
+import com.example.himmeltitting.fragments.SettingsFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +21,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapsFragment = Maps()
+        val favoritesFrafment = FavoritesFragment()
+        val settingsFragment = SettingsFragment()
+        val infoFragment = InfoFragment()
+
+        makeCuurentFragment(mapsFragment)
+
+        //legge til egen metode
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.search -> {
+                    makeCuurentFragment(mapsFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.fav -> {
+                    makeCuurentFragment(favoritesFrafment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.info -> {
+                    makeCuurentFragment(infoFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.settings -> {
+                    makeCuurentFragment(settingsFragment)
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
+
+    // initializes fragments from navigation bar
+    private fun makeCuurentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.mapsFragment, fragment)
+            commit()
+        }
+
+
 }
