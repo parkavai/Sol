1c8dc901c346b82b6ae4f8164a5a54534823370f
Liv Hilde Sjøflot <livhsj@online.no>
2022-05-11 20:41:40 +0200

    Comments Onboarding

--- app/src/main/java/com/example/soleklart/ui/onBoarding/OnBoardingItem.kt
+++ app/src/main/java/com/example/soleklart/ui/onBoarding/OnBoardingItem.kt
@@ -1,5 +1,8 @@
 package com.example.soleklart.ui.onBoarding
 
+/**
+ * Data class containing all items shown in the onboarding fragment.
+ */
 data class OnboardingItem(
     val onboardingImage: Int,
     val title: String,
