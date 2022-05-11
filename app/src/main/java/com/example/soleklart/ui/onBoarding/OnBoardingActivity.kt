9c1ff041a65d7aebd5eb50ec14c4941977d4aac8
Liv Hilde Sjøflot <livhsj@online.no>
2022-05-11 21:12:21 +0200

    updated pictures in onboarding & added link to airqual info in sunfacts

--- app/src/main/java/com/example/soleklart/ui/onBoarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/soleklart/ui/onBoarding/OnBoardingActivity.kt
@@ -38,22 +38,27 @@ class OnBoardingActivity : AppCompatActivity() {
         onboardingItemsAdapter = OnboardingItemsAdapter(
             listOf(
                 OnboardingItem(
-                    onboardingImage = R.drawable.search_image,
+                    onboardingImage = R.drawable.search_image2,
                     title = "Velg lokasjon for info om siktforhold",
                     description = "Her kan du søke i søkefeltet eller trykke på kartet"
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.results_image,
+                    onboardingImage = R.drawable.results_image2,
                     title = "Få info for valgt lokasjon",
                     description = "Her kan du se vær- og siktforhold under solnedgang og soloppgang"
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.favorite_image,
+                    onboardingImage = R.drawable.favorite_image2,
                     title = "Lagre dine favoritter",
                     description = "Her kan du slette og legge til, gøy!"
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.settings_image,
+                    onboardingImage = R.drawable.sunfacts_image2,
+                    title = "Les morsomme sunfacts!",
+                    description = "Her kan du lære om solstråler og andre fenomener"
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.settings_image2,
                     title = "Innstillinger",
                     description = "Bytt til darkmode, les spennende info om appen!"
                 ),
